19451e6f850942d411ab409d8cf4ca0e4bf46073
Javier Wilson <javier.wilson@gmail.com>
2013-03-13 10:39:15 -0300

    usa logger, corrige error al enviar correo de FAILED

--- tonto.sh
+++ tonto.sh
@@ -21,12 +21,12 @@ for ip in "${IPS[@]}"; do
 	else
 		rc_str="FAILED"
 		if [ -f $TMP/$ip.up ]; then
-			"$ip DOWN" | mail -s "$ip DOWN" "$EMAIL"
+			echo "$ip DOWN" | mail -s "$ip DOWN" "$EMAIL"
 			rm $TMP/$ip.up
 		fi
 		touch $TMP/$ip.down
 	fi
-	echo $ip $rc_str
+	logger $ip $rc_str
 done
 
 
