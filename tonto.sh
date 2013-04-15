b1cc8040f3dff8db538e05dbdbef4fe41ba4f96f
Javier Wilson <javier.wilson@gmail.com>
2013-04-15 08:03:47 -0400

    getting response time and using log file

--- tonto.sh
+++ tonto.sh
@@ -4,29 +4,39 @@
 # silly ip monitoring
 #
 
+LOG=/var/log/tonto
+IPS=( server1 server2 printer1 printer2 )
+EMAIL="root@example.com"
+PING_BIN=/usr/bin/ping
+PING_COUNT=1
+
 # read IP numbers from tonto.config.sh
 source `dirname $0`/tonto.config.sh
+mkdir -p $LOG
 
 # loop
 for ip in "${IPS[@]}"; do
-	ping -c 1 $ip 1>/dev/null
+	date=`date +%Y%m%d%H%M%S`
+	rt=`$PING_BIN -c $PING_COUNT $ip | tail -1| awk -F '/' '{print $5}'`
 	rc=$?
+	echo "$ip RC = $rc RT = $rt"
 	if [ $rc = 0 ]; then
 		rc_str="OK"
-		if [ -f $TMP/$ip.down ]; then
+		if [ -f $LOG/$ip.down ]; then
 			echo "$ip OK" | mail -s "$ip OK" "$EMAIL"
-			rm $TMP/$ip.down
+			rm $LOG/$ip.down
 		fi
-		touch $TMP/$ip.up
+		touch $LOG/$ip.up
 	else
 		rc_str="FAILED"
-		if [ -f $TMP/$ip.up ]; then
+		if [ -f $LOG/$ip.up ]; then
 			echo "$ip DOWN" | mail -s "$ip DOWN" "$EMAIL"
-			rm $TMP/$ip.up
+			rm $LOG/$ip.up
 		fi
-		touch $TMP/$ip.down
+		touch $LOG/$ip.down
 	fi
-	logger $ip $rc_str
+	echo "$date $ip $rc $rt" >> $LOG/tonto.log
+	logger "$date $ip $rc_str $rt"
 done
 
 
